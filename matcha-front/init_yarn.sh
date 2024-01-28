d18bce578b02fa7823ff1e3f24b5e423b8777943
jihkang <106561135+jihkang@users.noreply.github.com>
2024-01-28 16:53:47 +0900

    remove matcha

--- matcha-front/init_yarn.sh
+++
@@ -1,7 +0,0 @@
-npm install -global yarn
-yarn init -y
-yarn add react react-dom
-yarn add -D typescript @types/react @types/react-dom
-yarn tsc --init
-yarn add webpack --dev webpack-cli --dev
-yarn add html-webpack-plugin webpack-dev-server ts-loader --dev
