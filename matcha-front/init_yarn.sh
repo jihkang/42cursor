93abd20726921801727d14b01068cb2149409482
jihkang <106561135+jihkang@users.noreply.github.com>
2024-01-28 16:41:25 +0900

    added

---
+++ matcha-front/init_yarn.sh
@@ -0,0 +1,7 @@
+npm install -global yarn
+yarn init -y
+yarn add react react-dom
+yarn add -D typescript @types/react @types/react-dom
+yarn tsc --init
+yarn add webpack --dev webpack-cli --dev
+yarn add html-webpack-plugin webpack-dev-server ts-loader --dev
---
+++ matcha-front/src/page/App.tsx
@@ -0,0 +1,5 @@
+const App = () => {
+  return <>App contents</>;
+};
+
+export default App;
